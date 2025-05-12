d53caf65200a9b1c71c3f457ba74fae6008c4f25
LuisGR <lgordillor01@iesalbarregas.es>
2025-05-12 18:04:27 +0200

    Arreglar module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,6 @@ module com.proyecto {
     requires javafx.controls;
     requires javafx.fxml;
 
-    opens com.proyecto to javafx.fxml;
+    opens com.proyecto.Controlador to javafx.fxml;
     exports com.proyecto;
 }
